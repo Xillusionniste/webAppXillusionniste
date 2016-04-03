9b0af872a6cd36ba237c7f24360c9d69083e2d75
Xillusionniste <jules_corvers@hotmail.fr>
2016-04-03 11:27:35 +0200

    [m]

--- app/scripts/myApp.js
+++ app/scripts/myApp.js
@@ -56,6 +56,9 @@ var app = angular.module('myApp', [
         .when('/scoparules', {
             templateUrl: 'app/partials/scoparules.html',
         })
+        .when('/scopaprimera', {
+            templateUrl: 'app/partials/scopaprimera.html',
+        })
         .otherwise({
             redirectTo: '/home'
         });
